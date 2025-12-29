cb224cb840080f6504c0fff8666967986c20408c
levvis16 <zoomcross2@gmail.com>
2025-12-29 23:16:42 +0300

    add base logging

--- app/main.py
+++ app/main.py
@@ -1,12 +1,34 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.routers import cart, categories, orders, products, users, reviews
+from loguru import logger
 
+logger.add("info.log", format="Log: [{extra[log_id]}:{time} - {level} - {message}]", level="INFO", enqueue = True)
 # Создаём приложение FastAPI
 app = FastAPI(
     title="FastAPI Интернет-магазин",
     version="0.1.0",
 )
+
+from uuid import uuid4
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    log_id = str(uuid4())
+    with logger.contextualize(log_id=log_id):
+        try:
+            response = await call_next(request)
+            if response.status_code in [401, 402, 403, 404]:
+                logger.warning(f"Request to {request.url.path} failed")
+            else:
+                logger.info('Successfully accessed ' + request.url.path)
+        except Exception as ex:
+            logger.error(f"Request to {request.url.path} failed: {ex}")
+            response = JSONResponse(content={"success": False}, status_code=500)
+        return response
 
 app.mount("/media", StaticFiles(directory="media"), name="media")
 # Подключаем маршруты категорий и товаров
